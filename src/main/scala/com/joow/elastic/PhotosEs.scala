a01cb95acaa9469349c7b21d6c7c587ca1f402ab
samzhu <spike19820318@gmail.com>
2015-04-04 09:21:33 +0800

    add photo get

--- src/main/scala/com/joow/elastic/PhotosEs.scala
+++ src/main/scala/com/joow/elastic/PhotosEs.scala
@@ -1,6 +1,8 @@
 package com.joow.elastic
 
 
+import org.elasticsearch.action.get.GetResponse
+import org.elasticsearch.action.search.SearchResponse
 
 import scala.concurrent.Future
 
@@ -8,16 +10,26 @@ import scala.concurrent.Future
  * Created by SAM on 2015/4/3.
  */
 trait PhotosEs {
+
   import com.joow.entity.Photo
   import org.elasticsearch.action.index.IndexResponse
   import com.sksamuel.elastic4s.ElasticClient
   import com.sksamuel.elastic4s.ElasticDsl._
+
   private val type_name = "joow/photos"
 
-  protected def savePhotoEs(photo:Photo): Future[IndexResponse] ={
+  protected def savePhotoEs(photo: Photo): Future[IndexResponse] = {
     val client = ElasticClient.remote("127.0.0.1", 9300)
     val resp: Future[IndexResponse] = client.execute {
       index into type_name id photo.photoid doc photo
+    }
+    resp
+  }
+
+  protected def getPhotoEs(photoid: String): Future[GetResponse] = {
+    val client = ElasticClient.remote("127.0.0.1", 9300)
+    val resp: Future[GetResponse] = client.execute {
+      get id photoid from type_name
     }
     resp
   }
